8edd955c3d47219394ff61bd55d53550155306be
AhmetMcode <ahmethakan@mcodesoft.com>
2024-10-14 16:05:09 +0300

    jwt token oluşturuldu

---
+++ Core/MotorSocialApp.Application/Interfaces/Tokens/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using MotorSocialApp.Domain.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+
+namespace MotorSocialApp.Application.Interfaces.Tokens
+{
+    public interface ITokenService
+    {
+        Task<JwtSecurityToken> CreateToken(User user, IList<string> roles);
+
+        string GenerateRefreshToken();
+
+        ClaimsPrincipal? GetPrincipalFromExpiredToken();
+    }
+}
